e2eadf6e8c0889852279277948589128204fbbda
macandrew6 <mr.betterandrewmac@gmail.com>
2020-03-30 23:52:08 -0700

    refactors bench form

--- frontend/components/bench_form/bench_form.jsx
+++ frontend/components/bench_form/bench_form.jsx
@@ -1,36 +1,50 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { withRouter, Redirect } from "react-router-dom";
 import LoadingIcon from "../loading/loading_icon";
 
-class BenchForm extends Component {
-  constructor(props) {
-    super(props);
-    // render a loading icon when the bench is being created
-    this.state = {
-      description: "",
-      seating: 2,
-      photoFiles: [],
-      photoUrls: []
-    };
+const BenchForm = ({ loading, createBench, lat, lng, history }) => {
+  const [description, setDescription] = useState("");
+  const [seating, setSeating] = useState(2);
+  const [photoFiles, setPhotoFiles] = useState([]);
+  const [photoUrls, setPhotoUrls] = useState([]);
+  const descRef = useRef(null);
+  const seatingRef = useRef(null);
+  const submitRef = useRef(null);
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleFile = this.handleFile.bind(this);
-    this.navigateToSearch = this.navigateToSearch.bind(this);
-  }
+  useEffect(() => {
+    descRef.current.focus();
+  }, []);
 
-  navigateToSearch() {
-    this.props.history.push("/");
-  }
+  const descKeyDown = e => {
+    if (e.key === "Enter") {
+      seatingRef.current.focus();
+    }
+  };
 
-  update(field) {
+  const seatingKeyDown = e => {
+    if (e.key === "Enter") {
+      submitRef.current.focus();
+    }
+  };
+
+  const navigateToSearch = () => {
+    history.push("/");
+  };
+
+  const update = field => {
     return e => {
-      this.setState({
-        [field]: e.target.value
-      });
+      switch (field) {
+        case "description":
+          return setDescription(e.target.value);
+        case "seating":
+          return setSeating(e.target.value);
+        default:
+          return;
+      }
     };
-  }
+  };
 
-  handleFile(e) {
+  const handleFile = e => {
     const files = [];
     const urls = [];
     for (let i = 0; i < e.currentTarget.files.length; i++) {
@@ -39,123 +53,115 @@ class BenchForm extends Component {
       fileReader.onloadend = () => {
         files.push(file);
         urls.push(fileReader.result);
-        this.setState({
-          photoFiles: files,
-          photoUrls: urls
-        });
+        setPhotoFiles(files);
+        setPhotoUrls(urls);
       };
       if (file) {
         fileReader.readAsDataURL(file);
       }
     }
-  }
+  };
 
-  handleSubmit(e) {
+  const handleSubmit = e => {
     e.preventDefault();
     const formData = new FormData();
-    formData.append("bench[description]", this.state.description);
-    formData.append("bench[seating]", this.state.seating);
-    formData.append("bench[lat]", this.props.lat);
-    formData.append("bench[lng]", this.props.lng);
+    formData.append("bench[description]", description);
+    formData.append("bench[seating]", seating);
+    formData.append("bench[lat]", lat);
+    formData.append("bench[lng]", lng);
 
-    if (this.state.photoFiles !== []) {
-      this.state.photoFiles.forEach(photoFile => {
+    if (photoFiles !== []) {
+      photoFiles.forEach(photoFile => {
         formData.append("bench[photos][]", photoFile);
       });
     }
 
-    this.props.createBench(formData).then(res => {
+    createBench(formData).then(res => {
       let wildCard = res.bench.id;
-      this.props.history.push(`/benches/${wildCard}`);
+      history.push(`/benches/${wildCard}`);
     });
-    this.btn.setAttribute("disabled", true);
-    this.btn.classList.add("disabled");
+    submitRef.current.setAttribute("disabled", true);
+    submitRef.current.classList.add("disabled");
+  };
+
+  const preview =
+    photoUrls !== []
+      ? photoUrls.map((photoUrl, i) => (
+          <img
+            src={photoUrl}
+            key={i * performance.now()}
+            height="250px"
+            width="250px"
+          />
+        ))
+      : null;
+
+  if (loading) {
+    return <LoadingIcon className="bench-form-container" />;
   }
 
-  render() {
-    const { lat, lng, loading } = this.props;
-    const { seating, description } = this.state;
-    const preview =
-      this.state.photoUrls !== []
-        ? this.state.photoUrls.map((photoUrl, i) => (
-            <img
-              src={photoUrl}
-              key={i * performance.now()}
-              height="250px"
-              width="250px"
+  return (
+    <div>
+      <div className="bench-form-container">
+        <form className="bench-form" onSubmit={handleSubmit}>
+          <label htmlFor="description">
+            Description
+            <br />
+            <input
+              type="text"
+              ref={descRef}
+              onKeyDown={descKeyDown}
+              onChange={update("description")}
+              value={description}
+              required
             />
-          ))
-        : null;
+          </label>
+          <br />
+          <label htmlFor="number of seats">
+            Number of Seats
+            <br />
+            <input
+              min="0"
+              ref={seatingRef}
+              onKeyDown={seatingKeyDown}
+              type="number"
+              onChange={update("seating")}
+              value={seating}
+              required
+            />
+          </label>
+          <br />
+          <label htmlFor="latitude">
+            Latitude
+            <br />
+            <input type="number" disabled value={lat} />
+          </label>
+          <br />
+          <label htmlFor="longitude">
+            Longitude
+            <br />
+            <input type="number" disabled value={lng} />
+          </label>
 
-    if (loading) {
-      return <LoadingIcon className="bench-form-container" />;
-    }
-
-    return (
-      <div>
-        <div className="bench-form-container">
-          <form className="bench-form" onSubmit={this.handleSubmit}>
-            <label htmlFor="description">
-              Description
-              <br />
-              <input
-                type="text"
-                onChange={this.update("description")}
-                value={description}
-                required
-              />
-            </label>
-            <br />
-            <label htmlFor="number of seats">
-              Number of Seats
-              <br />
-              <input
-                min="0"
-                type="number"
-                onChange={this.update("seating")}
-                value={seating}
-                required
-              />
-            </label>
-            <br />
-            <label htmlFor="latitude">
-              Latitude
-              <br />
-              <input type="number" disabled value={lat} />
-            </label>
-            <br />
-            <label htmlFor="longitude">
-              Longitude
-              <br />
-              <input type="number" disabled value={lng} />
-            </label>
-
-            <div className="photo-input-container">
-              {preview}
-              <input
-                type="file"
-                multiple="multiple"
-                onChange={this.handleFile}
-              />
-            </div>
-            <br />
-            <button
-              className="btn"
-              ref={btn => {
-                this.btn = btn;
-              }}
-              type="submit"
-              multiple="multiple"
-            >
-              Create Bench
-            </button>
-            <br />
-            <button onClick={this.navigateToSearch}>Cancel</button>
-          </form>
-        </div>
+          <div className="photo-input-container">
+            {preview}
+            <input type="file" multiple="multiple" onChange={handleFile} />
+          </div>
+          <br />
+          <button
+            className="btn"
+            ref={submitRef}
+            type="submit"
+            multiple="multiple"
+          >
+            Create Bench
+          </button>
+          <br />
+          <button onClick={navigateToSearch}>Cancel</button>
+        </form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default withRouter(BenchForm);
