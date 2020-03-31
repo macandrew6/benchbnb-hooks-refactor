ec99a4d0ff8936c9156561d340038af130a56b74
macandrew6 <mr.betterandrewmac@gmail.com>
2020-03-30 21:17:19 -0700

    refactors session form to use hooks

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -1,81 +1,73 @@
-import React, { Component } from 'react'
+import React, { useState } from "react";
 
-class SessionForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: "",
-      password: ""
+const SessionForm = ({ formType, navLink, errors, processForm }) => {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const update = field => {
+    return e => {
+      switch (field) {
+        case "password":
+          return setPassword(e.target.value);
+        case "username":
+          return setUserName(e.target.value);
+        default:
+          return;
+      }
     };
+  };
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  update(field) {
-    return e => this.setState({
-      [field]: e.currentTarget.value
-    });
-  }
-
-  handleSubmit(e) {
+  const handleSubmit = e => {
     e.preventDefault();
-    const user = Object.assign({}, this.state);
-    this.props.processForm(user);
-    // clears the form after login in or signup
-    this.setState({
-      username: '',
-      password: ''
-    });
-  }
+    const user = Object.assign({}, { username, password });
+    processForm(user);
+  };
 
-  renderErrors() {
+  const renderErrors = () => {
     return (
-      <div>
-        {this.props.errors.map((error, i) => (
-          <div key={`error-${i}`}>
-            {error}
-          </div>
+      <ul>
+        {errors.map((error, i) => (
+          <li key={`error-${i}`}>{error}</li>
         ))}
-      </div>
+      </ul>
     );
-  }
+  };
 
-  render() {
-    return (
-      <div className="session-form-container">
-        <h2>Welcome to BenchBnB!</h2>
-        <form 
-          className="session-form"
-          onSubmit={this.handleSubmit}>
-          <br/>
-          <div className='login-form'>
-            <label>Username: 
-              <br/>
-              <input 
-                type="text"
-                value={this.state.username}
-                onChange={this.update('username')}
-              />
-            </label>
-            <br/>
-            <label>Password: 
-              <br/>
-              <input 
-                type="password"
-                value={this.state.password}
-                onChange={this.update('password')}
-              />
-            </label>
-            {this.renderErrors()}
-            <br/>
-            <h5>Please {this.props.formType} or {this.props.navLink}</h5>
-            <br/>
-            <button className="submit-button" type="submit"> {this.props.formType}</button>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="session-form-container">
+      <h2>Welcome to BenchBnB!</h2>
+      <form className="session-form" onSubmit={handleSubmit}>
+        <br />
+        <div className="login-form">
+          <label>
+            Username:
+            <br />
+            <input type="text" value={username} onChange={update("username")} />
+          </label>
+          <br />
+          <label>
+            Password:
+            <br />
+            <input
+              type="password"
+              value={password}
+              onChange={update("password")}
+            />
+          </label>
+          {renderErrors()}
+          <br />
+          <h5>
+            Please {formType} or {navLink}
+          </h5>
+          <br />
+          <button className="submit-button" type="submit">
+            {" "}
+            {formType}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SessionForm;
